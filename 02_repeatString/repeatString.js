698b303f7c18c8a87a0d1c900beb9243dcc81713
El-Burnie <173103025+El-Burnie@users.noreply.github.com>
2024-07-03 15:28:14 -0400

    Complete javascript exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, count) {
+    let repeatString = "";
+    if (count < 0 ) {
+        return "ERROR";
+    }
+    for (let i = 0; i < count; i++) {
+        repeatString += phrase;
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
