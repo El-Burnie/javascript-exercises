686a711c923925017f09a21779f04c130cea15c2
El-Burnie <173103025+El-Burnie@users.noreply.github.com>
2024-07-03 17:01:13 -0400

    Complete javascript exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(loNum, hiNum) {
+    if (!(typeof loNum === "number") || !(typeof hiNum === "number")) return "ERROR";
+    if (loNum < 0 || hiNum < 0) return "ERROR";
+    if (hiNum < loNum) [loNum, hiNum] = [hiNum, loNum];
+    let sum = 0;
+    for (let i = loNum; i < hiNum + 1; i++) { sum += i; }
+    return sum;
 };
 
 // Do not edit below this line
