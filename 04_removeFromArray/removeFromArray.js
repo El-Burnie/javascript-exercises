36c3cc8914c751d11c2f29194dcca6cba1ea5315
El-Burnie <173103025+El-Burnie@users.noreply.github.com>
2024-07-03 16:29:25 -0400

    Complete javascript exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+//  First attempt
+    const itemsToRemove = [];
+    for (let i = 1; i < arguments.length; i++) { 
+        itemsToRemove.push(arguments[i]);
+    }
+    for (let i = 0; i < array.length; i++) {
+        for (const item of itemsToRemove) {
+            if (item === array[i]) { 
+                array.splice(i, 1);
+                i -= 1;
+            }
+        }
+    }
+    return array;
+//  More efficient solution using Array methods:
+//  return array.filter(value => !args.includes(value));
 };
 
 // Do not edit below this line
