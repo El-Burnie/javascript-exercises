ef0a61da16677a5cf6a0c18a85cec34820561a6e
El-Burnie <173103025+El-Burnie@users.noreply.github.com>
2024-07-09 13:58:57 -0400

    Complete javascript exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1 = 0, num2 = 0) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1 = 0, num2 = 0) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum, value) => sum + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length === 0) return 0;
+  return array.reduce((product, value) => product * value, 1);
 };
 
-const power = function() {
-	
+const power = function(num1 = 0, num2 = 0) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num = 0) {
+	if (num === 0) return 1;
+  let factorial = 1;
+  for(let i = 1; i < num + 1; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
