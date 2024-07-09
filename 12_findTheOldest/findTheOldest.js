30e25e799e656c4cf5198f835e82666a05a60484
El-Burnie <173103025+El-Burnie@users.noreply.github.com>
2024-07-09 16:40:06 -0400

    Complete javascript exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const calculateAge = function (person) {
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function (people) {
+    const currentYear = (new Date()).getFullYear();
+    return people
+        .map((person) => {
+            if (!("yearOfDeath" in person)) person.yearOfDeath = currentYear;
+            return person;
+        })
+        .reduce((oldest, person) => {
+            return calculateAge(oldest) < calculateAge(person) ? person : oldest;
+        });  
 };
 
 // Do not edit below this line
