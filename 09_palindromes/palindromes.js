addd6106d6e100bd27a96e1589f8fde3738f7cb3
El-Burnie <173103025+El-Burnie@users.noreply.github.com>
2024-07-09 14:58:10 -0400

    Complete javascript exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = formatString(string);
+    let stringReversed = string.split("").reverse().join("");
+    return string === stringReversed;
 };
+
+function formatString(string) {
+    const validChars = "abcdefghijklmnopqrstuvwxyz0123456789"
+    return string
+    .toLowerCase()
+    .split("")
+    .filter(char => validChars.includes(char))
+    .join("");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
